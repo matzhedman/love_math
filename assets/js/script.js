4a5408f3f40ed83e0322e4b490fe607bc4c40859
matzhedman <hedmanmatz@gmail.com>
2023-01-03 07:49:56 +0000

    Add addeventlistener to game area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,49 @@
-console.log('connected!')
+// Wait for the DOM content to load before running the game
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);        
+            }
+        })
+    }
+    
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractAnswer() {
+
+}
+
+function displayMultiplyAnswer() {
+
+}
